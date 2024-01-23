26733bb0e88660baaac15b34e756b9297144c279
Mubarak <adesinamubarak123@gmail.com>
2024-01-23 05:45:07 -0800

    create navbar comp and overrite bootstrap style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
-    <div className="btn btn-primary">
-      test
+    <div>
+      <Navbar />
     </div>
   )
 }
