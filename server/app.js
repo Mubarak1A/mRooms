be38a56dda922ae31666c225be05a3d4e247ff25
Mubarak <adesinamubarak123@gmail.com>
2024-01-25 01:58:46 -0800

    connect to database

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const dbUrl = 'mongodb+srv://Mubarak:MuBaRak1@cluster0.kcl5drf.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(dbUrl)
+    .then(() => console.log('DB connected successfully!'))
+    .catch((err) => console.log(err))
 
 app.listen(8080, () => {
     console.log('Server running at port 8080');
