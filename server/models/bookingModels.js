08ff351928bfbb115ccb637ceb1f329357dc1d93
Mubarak <adesinamubarak123@gmail.com>
2024-02-16 09:50:58 -0800

    create the bookings model

---
+++ server/models/bookingModels.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = mongoose.Schema({
+    room : {
+        type : String, require : true
+    },
+    roomid : {
+        type : String, require : true
+    },
+    userid : {
+        type : String, require : true
+    },
+    fromdate : {
+        type : String, require : true
+    },
+    todate : {
+        type : String, require : true
+    },
+    toamount : {
+        type : Number, require : true
+    },
+    todays : {
+        type : Number, require : true
+    },
+    transactionid : {
+        type : String, require : true
+    },
+    status : {
+        type : String, require : true, default : "booked"
+    }
+},   {
+    timestamps : true
+});
+
+const bookingModel = mongoose.model('bookings', bookingSchema);
+
+module.exports = bookingModel;
---
+++ server/routes/bookingsRoutes.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const bookings = require('../models/bookingModels');
+
+const router = express.Router();
