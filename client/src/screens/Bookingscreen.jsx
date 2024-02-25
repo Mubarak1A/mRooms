6cc875f867b043fa2933d3c7d51b7b59fb8dbb9e
Mubarak <adesinamubarak123@gmail.com>
2024-02-24 16:03:39 -0800

    add stripe payment logic to backend

--- client/src/screens/Bookingscreen.jsx
+++ client/src/screens/Bookingscreen.jsx
@@ -24,7 +24,7 @@ function Bookingscreen() {
 
   const onToken = (token) => {
     console.log(token);
-    /*const bookingDetails = {
+    const bookingDetails = {
       room: rooms,
       userid: JSON.parse(localStorage.getItem("currentuser"))._id,
       fromDate,
@@ -39,7 +39,7 @@ function Bookingscreen() {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(bookingDetails),
-    });*/
+    });
   };
 
   const roomUrl = `http://localhost:8080/api/rooms/${id}`;
--- server/routes/bookingsRoutes.js
+++ server/routes/bookingsRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bookings = require('../models/bookingModels');
 const Room = require('../models/roomModels');
+const stripe = require('stripe')('sk_test_51OmfrQHLqMV9clnFqwbUKfsxH2Oq4gdZijv5QGktH91GlrxjKPlbrTD6XqKHCKNSISjiUXdu8RuToVhPmUs3mFs000tG2OBILA')
+const { v4: uuidv4 } = require('uuid');
 
 const router = express.Router();
 
@@ -13,48 +15,65 @@ router.post('/bookroom', (req, res) => {
         fromDate,
         toDate,
         totalAmount,
-        totalDays
+        totalDays,
+        token
     } = req.body;
 
     let newBooking;
 
-    // Step 1: Save the new booking
-    bookings.create({
-        room: room.name,
-        roomid: room._id,
-        userid,
-        fromdate: fromDate,
-        todate: toDate,
-        totalamount: totalAmount,
-        totaldays: totalDays,
-        transactionid: '1234'
+    const customer = stripe.customers.create({
+        email: token.email,
+        source: token.id
     })
-    .then((bookedRoom) => {
-        newBooking = bookedRoom;
+        .then((customer) => {
+            const payment = stripe.charges.create(
+                {
+                    amount: totalAmount * 100,
+                    customer: customer.id,
+                    currency: 'USD',
+                    receipt_email: token.mail
+                }, {
+                idempotencyKey: uuidv4()
+            }
+            )
+        })
+        .then(() => {
+            bookings.create({
+                room: room.name,
+                roomid: room._id,
+                userid,
+                fromdate: fromDate,
+                todate: toDate,
+                totalamount: totalAmount,
+                totaldays: totalDays,
+                transactionid: '1234'
+            })
+        })
+        .then((bookedRoom) => {
+            newBooking = bookedRoom;
 
-        // Step 2: Find the room and update the currentbookings array
-        return Room.findOneAndUpdate(
-            { _id: newBooking.roomid },
-            {
-                $push: {
-                    currentbookings: {
-                        bookingid: newBooking._id,
-                        fromdate: fromDate,
-                        todate: toDate,
-                        userid: userid,
-                        status: newBooking.status
+            return Room.findOneAndUpdate(
+                { _id: newBooking.roomid },
+                {
+                    $push: {
+                        currentbookings: {
+                            bookingid: newBooking._id,
+                            fromdate: fromDate,
+                            todate: toDate,
+                            userid: userid,
+                            status: newBooking.status
+                        }
                     }
-                }
-            },
-            { new: true }
-        );
-    })
-    .then((updatedRoom) => {
-        res.send('Booking Successful!');
-    })
-    .catch((err) => {
-        res.status(400).json({ err: err.message });
-    });
+                },
+                { new: true }
+            );
+        })
+        .then((updatedRoom) => {
+            res.send('Booking Successful!');
+        })
+        .catch((err) => {
+            res.status(400).json({ err: err.message });
+        });
 });
 
 module.exports = router;
