ca39252c3432047a807560f429e0badf6c112558
Mubarak <adesinamubarak123@gmail.com>
2024-03-06 14:28:14 -0800

    update homescreen styling

--- client/src/screens/Landingscreen.jsx
+++ client/src/screens/Landingscreen.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 
 function Landingscreen() {
   return (
-    <div className='row landing'>
-        <div className="col-md-9">
-            <h2 style={{color:'white', fontSize:'150px'}}>mRooms</h2>
-            <h3 style={{color:'white', fontSize:'48px'}}>Home away from home experience.</h3>
+    <div className='landing'>
+            <div className="container">
+              <h2>mRooms</h2>
+              <h3>Home away from home experience.</h3>
 
-            <Link to={'/home'}>
-                <button className='btn' style={{background:'white', width:'150px'}}>Get Started</button>
-            </Link>
-        </div>
+              <Link to={'/home'}>
+                  <button className='btn' style={{background:'white', width:'150px'}}>Get Started</button>
+              </Link>
+            </div>
     </div>
   )
 }
